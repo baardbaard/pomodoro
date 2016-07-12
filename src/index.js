2dd6d54f3684a7ae4f5c0821f796893b198765aa
Stef Thoen <stef@baardbaard.nl>
2016-07-12 20:50:21 +0200

    Port to Webpack
    Use webpack-hot-loader for BrowserSync functionality and use Babel so we
    can start using ES2015. Replace all ES5 code with ES2016.

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import Pomodoro from './Pomodoro';
+import React from 'react';
+import { render } from 'react-dom';
+
+render(<Pomodoro />, document.getElementById('root'));
--- src/pomodoro.js
+++ src/pomodoro.js
@@ -1,29 +1,23 @@
-// @todo: Port naar ES6
-// @todo: Use Webpack
-// @todo: Use Gulp + Browsersync
-// @todo: Add option to pause the timer
-// @todo: Let timer continue after goal has been reached
-// @todo: Duplicate timer to create  pause timer
-// @todo: Add option to activate pause timer after timer has been finished
-// @todo Track pomodoro's
-// @todo: Add better alerts (browser notifications?)
-// @todo: Add focus to input field
-// @todo: Replace input field with dropdown
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-var Pomodoro = React.createClass({
-    getInitialState: function() {
-        return {
+export default class Pomodoro extends React.Component {
+    constructor() {
+        super();
+        this.state = {
             time: 0,
             until: 0,
             enabled: true
-        };
-    },
+        }
+    }
 
-    type: function(e) {
-        this.setState({ until: e.target.value });
-    },
+    type(e) {
+        this.setState({
+            until: e.target.value
+        });
+    }
 
-    start: function() {
+    start() {
         this.setState({ enabled: false });
         this.interval = setInterval( () => {
             this.tick();
@@ -32,34 +26,36 @@ var Pomodoro = React.createClass({
                 this.finish();
             }
         }, 1000);
-    },
+    }
 
-    isTimeUp: function() {
+    isTimeUp() {
         return this.state.time == this.state.until;
-    },
+    }
 
-    finish: function() {
+    finish() {
         console.log('Ding ding ding!');
-        this.replaceState(this.getInitialState());
+        this.setState({
+            time: 0,
+            until: 0,
+            enabled: true
+        });
 
         ReactDOM.findDOMNode(this.refs.input).focus();
 
         return clearInterval(this.interval);
-    },
+    }
 
-    tick: function() {
+    tick() {
         this.setState({ time: this.state.time + 1 });
-    },
+    }
 
-    render: function() {
+    render() {
         return (
             <div>
-                <input ref="input" onChange={this.type} value={this.state.until} />
-                <button disabled={ ! this.state.enabled } onClick={this.start}>Go</button>
-                <h1>{this.state.time}</h1>
+                <input ref="input" onChange={ this.type.bind(this) } value={ this.state.until } />
+                <button disabled={ ! this.state.enabled } onClick={ this.start.bind(this) }>Go</button>
+                <h1>{ this.state.time }</h1>
             </div>
         );
     }
-});
-
-ReactDOM.render(<Pomodoro />, document.getElementById('app'));
+}
